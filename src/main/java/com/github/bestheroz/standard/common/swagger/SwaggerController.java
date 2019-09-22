a48e0325b9910ec2dc4439d3d7e687a12e363ef1
bestheroz <bestheroz@gmail.com>
2019-09-22 19:43:00 +0900

    소스정리

--- src/main/java/com/github/bestheroz/standard/common/swagger/SwaggerController.java
+++ src/main/java/com/github/bestheroz/standard/common/swagger/SwaggerController.java
@@ -2,8 +2,6 @@ package com.github.bestheroz.standard.common.swagger;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SwaggerController {
