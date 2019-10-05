d805c9c2199f158e673cb58862852cf75518ad6a
joony <joony@kakaopage.com>
2019-10-05 18:33:58 +0900

    환경변수 작업

--- vue/src/store.ts
+++ vue/src/store.ts
@@ -5,7 +5,11 @@ import snack from '@/components/snack/snack';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    appVersions: 'ver.191005',
+    host: process.env.VUE_APP_BASE_API_URL,
+    language: process.env.VUE_APP_LANGUAGE,
+  },
   mutations: {},
   actions: {},
   modules: {
