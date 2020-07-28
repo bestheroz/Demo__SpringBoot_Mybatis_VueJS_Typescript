47a93abfb8c5618b77692be993fe4e9f66fbbb1f
joony <bestheroz@gmail.com>
2020-07-28 23:11:20 +0900

    @NoArgsConstructor 제거

--- src/main/java/com/github/bestheroz/sample/api/entity/codegroup/TableCodeGroupEntity.java
+++ src/main/java/com/github/bestheroz/sample/api/entity/codegroup/TableCodeGroupEntity.java
@@ -1,7 +1,10 @@
 package com.github.bestheroz.sample.api.entity.codegroup;
 
 import com.github.bestheroz.sample.api.entity.AbstractCreatedUpdateEntity;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,7 +13,7 @@ import java.io.Serializable;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Entity(name = "CODE_GROUP")
 public class TableCodeGroupEntity extends AbstractCreatedUpdateEntity implements Serializable {
     private static final long serialVersionUID = 5295387617727505308L;
