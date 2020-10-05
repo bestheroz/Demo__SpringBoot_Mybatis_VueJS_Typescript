d878d8c01908077e0965a96332783334e8775209
dongjun Kim <bestheroz@gmail.com>
2020-10-05 15:41:33 +0900

    @RequestMapping(value = {"/", "/login", "/error"})

--- src/main/java/com/github/bestheroz/standard/common/controller/RootController.java
+++ src/main/java/com/github/bestheroz/standard/common/controller/RootController.java
@@ -2,13 +2,13 @@ package com.github.bestheroz.standard.common.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RootController implements ErrorController {
   private static final String INDEX_HTML = "index.html";
 
-  @GetMapping(value = { "/", "/login", "/error" })
+  @RequestMapping(value = {"/", "/login", "/error"})
   public String root() {
     return INDEX_HTML;
   }
