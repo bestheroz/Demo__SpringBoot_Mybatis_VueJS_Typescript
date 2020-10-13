eaa3de773594038ed79574dc7b9e094fa825d168
joony <joony@kakaopage.com>
2020-10-13 09:08:59 +0900

    index page

--- src/main/java/com/github/bestheroz/standard/common/controller/RootController.java
+++ src/main/java/com/github/bestheroz/standard/common/controller/RootController.java
@@ -3,12 +3,23 @@ package com.github.bestheroz.standard.common.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RootController implements ErrorController {
   private static final String INDEX_HTML = "index.html";
 
-  @RequestMapping(value = {"/", "/login", "/error"})
+  @RequestMapping(
+    value = {"/", "/login", "/error"},
+    method = {
+      RequestMethod.OPTIONS,
+      RequestMethod.GET,
+      RequestMethod.POST,
+      RequestMethod.PUT,
+      RequestMethod.PATCH,
+      RequestMethod.DELETE,
+    }
+  )
   public String root() {
     return INDEX_HTML;
   }
