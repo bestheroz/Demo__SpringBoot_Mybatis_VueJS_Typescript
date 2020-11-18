61cf9afcb7729602649be0e1d6730eba62f1424a
dongjun Kim <bestheroz@gmail.com>
2020-11-19 02:56:02 +0900

    extractCss

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   parallel: process.env.NODE_ENV !== "production",
   transpileDependencies: ["vuetify"],
+  css: {
+    extract:
+      /* eslint-disable indent */
+      process.env.NODE_ENV === "production"
+        ? {
+            ignoreOrder: true,
+          }
+        : false,
+    /* eslint-enable indent */
+  },
 };
