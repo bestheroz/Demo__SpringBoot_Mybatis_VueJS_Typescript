b961220b2003dadf31522c78fd6123ff90b57c4d
joony <joony@kakaopage.com>
2021-10-29 22:24:50 +0900

    @ToString

--- src/main/java/com/github/bestheroz/standard/common/response/ApiResult.java
+++ src/main/java/com/github/bestheroz/standard/common/response/ApiResult.java
@@ -3,9 +3,11 @@ package com.github.bestheroz.standard.common.response;
 import com.github.bestheroz.standard.common.exception.ExceptionCode;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class ApiResult<T> {
   private String code;
   private String message;
