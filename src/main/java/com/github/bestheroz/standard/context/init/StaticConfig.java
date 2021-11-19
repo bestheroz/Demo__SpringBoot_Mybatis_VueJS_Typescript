96513da5d128c315135275d9d1d1f557e3518cdf
joony <joony@kakaopage.com>
2021-11-19 12:51:47 +0900

    @Bean

--- src/main/java/com/github/bestheroz/standard/context/init/StaticConfig.java
+++ src/main/java/com/github/bestheroz/standard/context/init/StaticConfig.java
@@ -3,7 +3,7 @@ package com.github.bestheroz.standard.context.init;
 import com.github.bestheroz.standard.common.util.AccessBeanUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
@@ -12,7 +12,7 @@ import org.springframework.core.env.Environment;
 public class StaticConfig {
   public static Boolean LOCAL_ACTIVE_PROFILE_FLAG = null;
 
-  @Autowired
+  @Bean
   public void setConstant() {
     if (LOCAL_ACTIVE_PROFILE_FLAG == null) {
       LOCAL_ACTIVE_PROFILE_FLAG =
