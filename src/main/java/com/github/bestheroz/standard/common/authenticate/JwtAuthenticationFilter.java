6feda6f02f6e4c05206d20f88b7e0bbae02fa267
joony <joony@kakaopage.com>
2021-11-21 23:05:43 +0900

    && requestURI.startsWith("/api")

--- src/main/java/com/github/bestheroz/standard/common/authenticate/JwtAuthenticationFilter.java
+++ src/main/java/com/github/bestheroz/standard/common/authenticate/JwtAuthenticationFilter.java
@@ -41,7 +41,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
             .map(item -> item.replace("*", ""))
             .filter(requestURI::startsWith)
             .findFirst();
-    if (oPublicPages.isEmpty()) {
+    if (oPublicPages.isEmpty() && requestURI.startsWith("/api")) {
       final String token = JwtTokenProvider.resolveAccessToken(request);
       if (StringUtils.isEmpty(token)) {
         log.info("non accessToken");
