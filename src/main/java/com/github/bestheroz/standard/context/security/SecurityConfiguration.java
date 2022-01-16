2868c492c43c68f5d8e0ddbc66bd0975ef60b569
joony <joony.kim@kakaoent.com>
2022-01-16 22:40:50 +0900

    fixxx

--- src/main/java/com/github/bestheroz/standard/context/security/SecurityConfiguration.java
+++ src/main/java/com/github/bestheroz/standard/context/security/SecurityConfiguration.java
@@ -19,11 +19,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   public static final String[] PUBLIC =
       new String[] {
-        "/api/sign-in",
-        "/api/sign-in/**",
-        "/api/sign-out",
-        "/api/auth/refresh-token",
-        "/actuator/**",
+        "/api/sign-in", "/api/sign-in/refresh-token", "/api/sign-out", "/actuator/**",
       };
 
   @Override
